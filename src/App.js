f380b1f3e7aefad15f9a98954280147826b7d5ee
Haris <harisgujjar317@gmail.com>
2024-02-20 20:21:37 -0500

    UseState hoook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  // const [state, setState] = useState({count: 4, theme:"blue"});
+  // const count = state.count;
+  // const theme = state.theme;
+
+  // we can use two state to define value seperatly to do our task rather than defining values seperaly.
+  // forexample
+
+  const [count, setCount] = useState(1);
+  const [theme, setTheme] = useState('blue');
+
+  const function1 = () => {
+    setCount((prevState) => prevState + 1);
+    setTheme('red');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <div>
+        <button onClick={function1}>+</button> 
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
