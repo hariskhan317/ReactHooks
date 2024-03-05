c29b91995049a65a367607b33b0c7bf474c697d9
Haris <harisgujjar317@gmail.com>
2024-03-05 16:05:31 -0500

    Code for useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
 
+const ACTIONS = {
+  INCREMENT: 'incrementedDispatch',
+  DECREMENT : 'decrementedDispatch',
+}
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1};
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1};
+    default:
+      return state
+  }
+}
 function App() {
+  const [state, displatch]  = useReducer(reducer, { count: 0 });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => displatch({type: 'incrementedDispatch'})}>+</button>
+      <div>{state.count}</div>
+      <button onClick={() => displatch({type: 'decrementedDispatch'})}>-</button>
     </div>
   );
 }
